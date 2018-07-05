4ad73e7ee8aada68c2454d924a7ac87bbb5d0341
ratmadja <ratmadja@gmail.com>
2018-07-05 14:03:03 -0700

    04Responsive

---
+++ scripts/master.js
@@ -0,0 +1,13 @@
+// Start of what makes cover div clickable
+
+$(".work-NimbusDashCover").click (function () {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
+
+$(".work-NimbusLandingCover").click (function () {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
+
+// Start of what makes cover div clickable
