f31b78d9c612c7a5cec791739b6fa8f584823970
ratmadja <ratmadja@gmail.com>
2018-07-05 19:18:45 -0700

    visual refinement homepage

--- scripts/master.js
+++ scripts/master.js
@@ -10,4 +10,19 @@ $(".work-NimbusLandingCover").click (function () {
   return false;
 });
 
+$(".pastwork-ArchCover").click (function () {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
+
+$(".pastwork-FurnitureCover").click (function () {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
+
+$(".pastwork-GraphicsCover").click (function () {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
+
 // Start of what makes cover div clickable
